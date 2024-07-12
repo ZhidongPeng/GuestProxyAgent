8de5270fb07ced130594f8f3b6dd72d4e49149fd
Zhidong Peng <zpeng@microsoft.com>
2024-07-12 11:55:07 -0700

    resolve comments

--- proxy_agent/src/redirector.rs
+++ proxy_agent/src/redirector.rs
@@ -21,8 +21,6 @@ pub use linux::BpfObject;
 #[cfg(windows)]
 pub use windows::BpfObject;
 /// This is a workaround to allow BpfObject to be sent between threads.
-pub struct RedirectorObject(pub BpfObject);
-unsafe impl Send for RedirectorObject {}
 
 #[derive(Serialize, Deserialize)]
 #[repr(C)]
